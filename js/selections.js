2d4a1ba28eb2789335e1d4f5525d761033ab102b
rc-ops <rodrigocarvalhocontato@protonmail.com>
2022-10-18 13:12:21 -0300

    Empties selection when a smaller flavor

--- js/selections.js
+++ js/selections.js
@@ -1,15 +1,19 @@
 function hideShowFlavors() {
     if (document.getElementById('tamanho-grande').checked) {
-        document.getElementById('sabor2').style.display = '';
-        document.getElementById('sabor3').style.display = '';
+        document.getElementById('sabores2').style.display = '';
+        document.getElementById('sabores3').style.display = '';
     } else if (document.getElementById('tamanho-medio').checked) {
-        document.getElementById('sabor2').style.display = '';
-        document.getElementById('sabor3').style.display = 'none';
-        document.getElementById('sabor3').selectedIndex = 0;
+        document.getElementById('sabores2').style.display = '';
+        document.getElementById('sabores3').style.display = 'none';
+        emptySelection('sabor3')
     } else if (document.getElementById('tamanho-pequeno').checked) {
-        document.getElementById('sabor2').style.display = 'none';
-        document.getElementById('sabor2').selectedIndex = 0;
-        document.getElementById('sabor3').style.display = 'none';
-        document.getElementById('sabor3').selectedIndex = 0;
+        document.getElementById('sabores2').style.display = 'none';
+        emptySelection('sabor2')
+        document.getElementById('sabores3').style.display = 'none';
+        emptySelection('sabor3')
     }
+}
+
+function emptySelection(elementName) {
+    document.getElementById(elementName).selectedIndex = '0';
 }
